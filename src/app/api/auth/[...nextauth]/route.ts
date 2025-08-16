3884a0cfb839ddbe9ab16e919e33758237e97733
steveMuema <hitchs3x@gmail.com>
2025-08-16 11:06:45 +0300

    fix(auth): separate export of authOptions for clarity

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -80,4 +80,5 @@ const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
+export { authOptions }; // Export authOptions for use in other files
